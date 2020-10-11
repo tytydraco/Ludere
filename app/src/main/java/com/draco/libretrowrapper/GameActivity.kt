e6a99b2f2f0209e0c797c3ef44604fd51ba79187
tytydraco <tylernij@gmail.com>
2020-10-11 15:46:40 -0700

    Don't recreate activity on rotate / sleep
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/libretrowrapper/GameActivity.kt
+++ app/src/main/java/com/draco/libretrowrapper/GameActivity.kt
@@ -4,7 +4,6 @@ import android.app.UiModeManager
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
