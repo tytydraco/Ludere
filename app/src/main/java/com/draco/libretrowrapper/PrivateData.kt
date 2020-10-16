02fc70b51bf10c72cf19bec1494c56e2a14f35a0
tytydraco <tylernij@gmail.com>
2020-10-16 01:11:18 -0700

    Store core in permanent location
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/libretrowrapper/PrivateData.kt
+++ app/src/main/java/com/draco/libretrowrapper/PrivateData.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 class PrivateData(context: Context) {
     val rom = File("${context.filesDir.absolutePath}/rom")
-    val core = File("${context.cacheDir.absolutePath}/core")
+    val core = File("${context.filesDir.absolutePath}/core")
     val save = File("${context.filesDir.absolutePath}/save")
     val state = File("${context.filesDir.absolutePath}/state")
     val savedInstanceState = File("${context.cacheDir.absolutePath}/saved_instance_state")
