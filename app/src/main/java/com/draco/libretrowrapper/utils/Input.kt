e4ad2e52203de22fa782ccedd9c2773468039096
tytydraco <tylernij@gmail.com>
2020-10-18 20:18:32 -0700

    Allow more keycodes
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/libretrowrapper/utils/Input.kt
+++ app/src/main/java/com/draco/libretrowrapper/utils/Input.kt
@@ -16,7 +16,11 @@ class Input {
         KeyEvent.KEYCODE_DPAD_DOWN,
         KeyEvent.KEYCODE_DPAD_RIGHT,
         KeyEvent.KEYCODE_BUTTON_L1,
+        KeyEvent.KEYCODE_BUTTON_L2,
         KeyEvent.KEYCODE_BUTTON_R1,
+        KeyEvent.KEYCODE_BUTTON_R2,
+        KeyEvent.KEYCODE_BUTTON_THUMBL,
+        KeyEvent.KEYCODE_BUTTON_THUMBR,
         KeyEvent.KEYCODE_BUTTON_START,
         KeyEvent.KEYCODE_BUTTON_SELECT
     )
