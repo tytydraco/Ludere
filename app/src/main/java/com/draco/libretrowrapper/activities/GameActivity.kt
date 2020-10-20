87f1812b940aa065f6686db0da763fa2adf5c228
tytydraco <tylernij@gmail.com>
2020-10-19 17:29:27 -0700

    Remove unused import
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/libretrowrapper/activities/GameActivity.kt
+++ app/src/main/java/com/draco/libretrowrapper/activities/GameActivity.kt
@@ -4,7 +4,6 @@ import android.app.AlertDialog
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
