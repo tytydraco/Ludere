c39f67ffc50efcc5546aaf4b3c4cf4146f6d3da2
tytydraco <tylernij@gmail.com>
2020-10-21 17:45:41 -0700

    Use separate function to handle custom GamePad keys
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/libretrowrapper/utils/GamePad.kt
+++ app/src/main/java/com/draco/libretrowrapper/utils/GamePad.kt
@@ -16,20 +16,34 @@ class GamePad(
     val pad: RadialGamePad = RadialGamePad(padConfig, 0f, context)
     private val compositeDisposable = CompositeDisposable()
 
+    private fun overrideButtonEvent(event: Event.Button, retroView: GLRetroView): Boolean {
+        /* We only accept down key events */
+        if (event.action != KeyEvent.ACTION_DOWN)
+            return false
+
+        /* If we recognize the event ID, handle it */
+        when (event.id) {
+            Input.KEYCODE_SAVE_STATE -> RetroViewUtils.saveState(retroView, privateData)
+            Input.KEYCODE_LOAD_STATE -> RetroViewUtils.loadState(retroView, privateData)
+
+            /* ID unrecognized, return false */
+            else -> return false
+        }
+
+        /* We handled the event */
+        return true
+    }
+
     private fun eventHandler(event: Event, retroView: GLRetroView) {
         when (event) {
             is Event.Button -> {
-                when (event.id) {
-                    Input.KEYCODE_SAVE_STATE -> {
-                        if (event.action == KeyEvent.ACTION_DOWN)
-                            RetroViewUtils.saveState(retroView, privateData)
-                    }
-                    Input.KEYCODE_LOAD_STATE -> {
-                        if (event.action == KeyEvent.ACTION_DOWN)
-                            RetroViewUtils.loadState(retroView, privateData)
-                    }
-                    else -> retroView.sendKeyEvent(event.action, event.id)
-                }
+                /*
+                 * Attempt to override the event to handle unorthodox keycodes, such as save
+                 * and load states. If the override handler returns false, the event was not
+                 * overridden, so we should pass the event to the GLRetroView as-is.
+                 */
+                if (!overrideButtonEvent(event, retroView))
+                    retroView.sendKeyEvent(event.action, event.id)
             }
             is Event.Direction -> {
                 when (event.id) {
