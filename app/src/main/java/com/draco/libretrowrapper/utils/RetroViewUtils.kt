3ed1bbe272abbe84663be99162568c9ae619fafc
tytydraco <tylernij@gmail.com>
2020-10-21 22:47:26 -0700

    Save states asynchronously
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/libretrowrapper/utils/RetroViewUtils.kt
+++ app/src/main/java/com/draco/libretrowrapper/utils/RetroViewUtils.kt
@@ -5,7 +5,9 @@ import com.swordfish.libretrodroid.GLRetroView
 class RetroViewUtils {
     companion object {
         fun saveState(retroView: GLRetroView, privateData: PrivateData) {
-            privateData.state.writeBytes(retroView.serializeState())
+            Thread {
+                privateData.state.writeBytes(retroView.serializeState())
+            }.start()
         }
 
         fun loadState(retroView: GLRetroView, privateData: PrivateData) {
@@ -18,12 +20,14 @@ class RetroViewUtils {
         }
 
         fun saveTempState(retroView: GLRetroView, privateData: PrivateData) {
-            /* Save a temporary state since Android killed the activity */
-            val savedInstanceStateBytes = retroView.serializeState()
-            with (privateData.savedInstanceState.outputStream()) {
-                write(savedInstanceStateBytes)
-                close()
-            }
+            Thread {
+                /* Save a temporary state since Android killed the activity */
+                val savedInstanceStateBytes = retroView.serializeState()
+                with (privateData.savedInstanceState.outputStream()) {
+                    write(savedInstanceStateBytes)
+                    close()
+                }
+            }.start()
         }
 
         fun restoreTempState(retroView: GLRetroView, privateData: PrivateData) {
