ada9ab3f4c1f240cd25b95b40eb0b705f1ae6b96
tytydraco <tylernij@gmail.com>
2020-10-23 20:59:30 -0700

    Allow core to be specified
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- tools/auto.py
+++ tools/auto.py
@@ -1,6 +1,11 @@
 import xml.etree.ElementTree as ET
-import os, shutil
-import re
+import os, shutil, sys, re
+
+if (len(sys.argv) < 2):
+    print('No core specified')
+    exit(1)
+
+core = sys.argv[1]
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -11,7 +16,7 @@ for file in os.listdir('input'):
     romname = os.path.splitext(file)[0]
     romext = os.path.splitext(file)[1]
     romid = re.sub(r'[^A-Za-z0-9]+', '', romname)
-    romcore = 'nestopia'
+    romcore = core
 
     tree = ET.parse('../app/src/main/res/values/config.xml')
     root = tree.getroot()
