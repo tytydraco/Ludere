8c6d686be80b24d6f002add0b789f01cd3b4c9db
tytydraco <tylernij@gmail.com>
2020-10-28 11:31:29 -0700

    Recurse into autogen inputs
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- autogen/generate.py
+++ autogen/generate.py
@@ -1,45 +1,48 @@
 import xml.etree.ElementTree as ET
 import os, shutil, sys, re
 
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+scriptabspath = os.path.abspath(__file__)
+cwdabspath = os.path.dirname(scriptabspath)
+os.chdir(cwdabspath)
 
-if not os.path.isdir('../system'):
-    os.mkdir('../system')
+rootdir = os.path.abspath('..')
 
-shutil.copy('../app/src/main/res/values/config.xml', 'config.tmp.txt')
-for file in os.listdir('input'):
-    if not os.path.isfile(f'input/{file}'):
+if not os.path.isdir(f'{rootdir}/system'):
+    os.mkdir(f'{rootdir}/system')
+
+shutil.copy(f'{rootdir}/app/src/main/res/values/config.xml', f'{cwdabspath}/config.tmp.txt')
+for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
+    if not os.path.isfile(f'{subdir}/config.xml'):
         continue
-    if file == '.gitignore':
-        continue
-    if file == 'config.xml':
-        continue
+    for file in os.listdir(subdir):
+        if not os.path.isfile(f'{subdir}/{file}'):
+            continue
+        if file == '.gitignore':
+            continue
+        if file == 'config.xml':
+            continue
 
-    romname = os.path.splitext(file)[0]
-    romext = os.path.splitext(file)[1]
-    romid = re.sub(r'[^A-Za-z0-9]+', '', romname)
+        romname = os.path.splitext(file)[0]
+        romext = os.path.splitext(file)[1]
+        romid = re.sub(r'[^A-Za-z0-9]+', '', romname)
 
-    tree = ET.parse('input/config.xml')
-    root = tree.getroot()
+        tree = ET.parse(f'{subdir}/config.xml')
+        root = tree.getroot()
 
-    shutil.copy(f'input/{file}', '../system/rom')
-    for element in root.iter('string'):
-        if 'config_rom_id' in element.attrib.get('name'):
-            element.text = romid
-        if 'config_name' in element.attrib.get('name'):
-            element.text = romname
-    tree.write('../app/src/main/res/values/config.xml')
-    os.chdir('..')
-    
-    print(f' * Building {romname}...')
-    if os.name == 'nt':
-        os.system('gradlew assembleRelease')
-    else:
-        os.system('./gradlew assembleRelease')
-    os.chdir(dname)
+        shutil.copy(f'{subdir}/{file}', f'{rootdir}/system/rom')
+        for element in root.iter('string'):
+            if 'config_rom_id' in element.attrib.get('name'):
+                element.text = romid
+            if 'config_name' in element.attrib.get('name'):
+                element.text = romname
+        tree.write(f'{rootdir}/app/src/main/res/values/config.xml')
+        
+        print(f' * Building {romname}...')
 
-    shutil.copy('../app/build/outputs/apk/release/app-release.apk', f'output/{romid}.apk')
+        os.chdir(rootdir)
+        os.system(f'{rootdir}/gradlew assembleRelease')
+        os.chdir(cwdabspath)
+        
+        shutil.copy(f'{rootdir}/app/build/outputs/apk/release/app-release.apk', f'{cwdabspath}/output/{romid}.apk')
 
-shutil.move('config.tmp.txt', '../app/src/main/res/values/config.xml')
+shutil.move(f'{cwdabspath}/config.tmp.txt', f'{rootdir}/app/src/main/res/values/config.xml')
