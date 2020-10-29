8ae53b6036339e2855b0b7f74a82980740e3a204
tytydraco <tylernij@gmail.com>
2020-10-28 22:59:27 -0700

    Track progress of autogen builds
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- autogen/generate.py
+++ autogen/generate.py
@@ -11,6 +11,8 @@ if not os.path.isdir(f'{rootdir}/system'):
     os.mkdir(f'{rootdir}/system')
 
 shutil.copy(f'{rootdir}/app/src/main/res/values/config.xml', f'{cwdabspath}/config.tmp.txt')
+
+buildsize = 0
 for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
     if not os.path.isfile(f'{subdir}/config.xml'):
         continue
@@ -21,6 +23,25 @@ for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
             continue
         if file == 'config.xml':
             continue
+    buildsize += 1
+
+for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
+    if not os.path.isfile(f'{subdir}/config.xml'):
+        continue
+    for file in os.listdir(subdir):
+        if not os.path.isfile(f'{subdir}/{file}'):
+            continue
+        if file == '.gitignore':
+            continue
+        if file == 'config.xml':
+            continue
+
+        print('')
+        print(f' *****************************************')
+        print(f' * BUILDS REMAINING: {buildsize}')
+        print(f' * BUILDING: {file}')
+        print(f' *****************************************')
+        print('')
 
         romname = os.path.splitext(file)[0]
         romext = os.path.splitext(file)[1]
@@ -37,12 +58,11 @@ for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
                 element.text = romname
         tree.write(f'{rootdir}/app/src/main/res/values/config.xml')
         
-        print(f' * Building {romname}...')
-
         os.chdir(rootdir)
         os.system(f'{rootdir}/gradlew assembleRelease')
         os.chdir(cwdabspath)
         
         shutil.copy(f'{rootdir}/app/build/outputs/apk/release/app-release.apk', f'{cwdabspath}/output/{romid}.apk')
 
+        buildsize -= 1
 shutil.move(f'{cwdabspath}/config.tmp.txt', f'{rootdir}/app/src/main/res/values/config.xml')
