725f48e489b7ea54f63eaabd7717003ce3786ab6
tytydraco <tylernij@gmail.com>
2020-11-01 19:29:24 -0800

    Add exit button to menu
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/ludere/utils/Menu.kt
+++ app/src/main/java/com/draco/ludere/utils/Menu.kt
@@ -1,41 +1,43 @@
 package com.draco.ludere.utils
 
-import android.content.Context
+import android.app.Activity
 import android.content.DialogInterface
 import androidx.appcompat.app.AlertDialog
 import com.draco.ludere.R
 import com.swordfish.libretrodroid.GLRetroView
 
 class Menu(
-    private val context: Context,
+    private val activity: Activity,
     private val retroView: GLRetroView
 ) {
-    private val privateData = PrivateData(context)
+    private val privateData = PrivateData(activity)
 
     private val menuOptions = listOfNotNull(
-        context.getString(R.string.menu_save_state),
-        context.getString(R.string.menu_load_state),
-        context.getString(R.string.menu_mute),
-        context.getString(R.string.menu_fast_forward),
-        context.getString(R.string.menu_next_disc).takeIf { retroView.getAvailableDisks() > 0 },
-        context.getString(R.string.menu_previous_disk).takeIf { retroView.getAvailableDisks() > 0 }
+        activity.getString(R.string.menu_exit),
+        activity.getString(R.string.menu_save_state),
+        activity.getString(R.string.menu_load_state),
+        activity.getString(R.string.menu_mute),
+        activity.getString(R.string.menu_fast_forward),
+        activity.getString(R.string.menu_next_disc).takeIf { retroView.getAvailableDisks() > 0 },
+        activity.getString(R.string.menu_previous_disk).takeIf { retroView.getAvailableDisks() > 0 }
     ).toTypedArray()
 
     private inner class MenuOnClickListener : DialogInterface.OnClickListener {
         override fun onClick(dialog: DialogInterface?, which: Int) {
             when (menuOptions[which]) {
-                context.getString(R.string.menu_save_state) -> RetroViewUtils.saveState(retroView, privateData)
-                context.getString(R.string.menu_load_state) -> RetroViewUtils.loadState(retroView, privateData)
-                context.getString(R.string.menu_mute) -> RetroViewUtils.toggleMute(retroView)
-                context.getString(R.string.menu_fast_forward) -> RetroViewUtils.toggleFastForward(retroView)
-                context.getString(R.string.menu_next_disc) -> RetroViewUtils.nextDisc(retroView)
-                context.getString(R.string.menu_previous_disk) -> RetroViewUtils.previousDisc(retroView)
+                activity.getString(R.string.menu_exit) -> activity.finishAffinity()
+                activity.getString(R.string.menu_save_state) -> RetroViewUtils.saveState(retroView, privateData)
+                activity.getString(R.string.menu_load_state) -> RetroViewUtils.loadState(retroView, privateData)
+                activity.getString(R.string.menu_mute) -> RetroViewUtils.toggleMute(retroView)
+                activity.getString(R.string.menu_fast_forward) -> RetroViewUtils.toggleFastForward(retroView)
+                activity.getString(R.string.menu_next_disc) -> RetroViewUtils.nextDisc(retroView)
+                activity.getString(R.string.menu_previous_disk) -> RetroViewUtils.previousDisc(retroView)
             }
         }
     }
 
     fun show() {
-        AlertDialog.Builder(context)
+        AlertDialog.Builder(activity)
             .setItems(menuOptions, MenuOnClickListener())
             .show()
     }
