7340121ee25e2a226a9d2d1a7d4b118d51a8127d
tytydraco <tylernij@gmail.com>
2020-11-04 23:26:59 -0800

    Store system.bin as a raw resource
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/ludere/utils/System.kt
+++ app/src/main/java/com/draco/ludere/utils/System.kt
@@ -1,27 +1,20 @@
 package com.draco.ludere.utils
 
 import android.content.Context
+import com.draco.ludere.R
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 import java.io.File
 
 class System(private val context: Context) {
-    companion object {
-        const val SYSTEM_BIN_NAME = "system.bin"
-    }
-
     fun extractToFilesDir() {
         /* Bail if we have already populated our assets folder */
         if (!context.filesDir.listFiles().isNullOrEmpty())
             return
 
-        /* Bail if we don't even have a system.bin tarball */
-        if (context.assets.list("")?.contains(SYSTEM_BIN_NAME) == false)
-            return
-
         try {
             /* Iterate over all tarred items */
-            context.assets.open(SYSTEM_BIN_NAME).use { systemTarInputStream ->
+            context.resources.openRawResource(R.raw.system).use { systemTarInputStream ->
                 GzipCompressorInputStream(systemTarInputStream).use { gzipCompressorInputStream ->
                     TarArchiveInputStream(gzipCompressorInputStream).use { tarArchiveInputStream ->
                         while (true) {
