a1ec70359f0a9725c99228d2687e234ff6944f47
tytydraco <tylernij@gmail.com>
2021-03-22 10:51:06 -0700

    Update autogen
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- autogen/generate.py
+++ autogen/generate.py
@@ -1,20 +1,14 @@
 import xml.etree.ElementTree as ET
 import os, shutil, sys, re
 
-scriptabspath = os.path.abspath(__file__)
-cwdabspath = os.path.dirname(scriptabspath)
-os.chdir(cwdabspath)
-
 rootdir = os.path.abspath('..')
 
-if not os.path.isdir(f'{rootdir}/app/src/main/res/raw'):
-    os.mkdir(f'{rootdir}/app/src/main/res/raw')
+if not os.path.isdir('../app/src/main/res/raw'):
+    os.mkdir('../app/src/main/res/raw')
 
-shutil.copy(f'{rootdir}/app/src/main/res/values/config.xml', f'{cwdabspath}/config.tmp.txt')
-
-buildsize = 0
-for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
+for subdir in [x[0] for x in os.walk('input')]:
     if not os.path.isfile(f'{subdir}/config.xml'):
+        print(f'{subdir} does not contain a config.xml file. Skipping...')
         continue
     for file in os.listdir(subdir):
         if not os.path.isfile(f'{subdir}/{file}'):
@@ -23,34 +17,16 @@ for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
             continue
         if file == 'config.xml':
             continue
-        buildsize += 1
-
-for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
-    if not os.path.isfile(f'{subdir}/config.xml'):
-        continue
-    for file in os.listdir(subdir):
-        if not os.path.isfile(f'{subdir}/{file}'):
-            continue
-        if file == '.gitignore':
-            continue
-        if file == 'config.xml':
-            continue
-
-        print('')
-        print(f' *****************************************')
-        print(f' * BUILDS REMAINING: {buildsize}')
-        print(f' * BUILDING: {file}')
-        print(f' *****************************************')
-        print('')
 
         romname = os.path.splitext(file)[0]
         romext = os.path.splitext(file)[1]
-        romid = re.sub(r'[^A-Za-z0-9]+', '', romname)
+        romid = romname.lower()
+        romid = re.sub(r'[^A-Za-z0-9]+', '', romid)
 
         tree = ET.parse(f'{subdir}/config.xml')
         root = tree.getroot()
 
-        shutil.copy(f'{subdir}/{file}', f'{rootdir}/app/src/main/res/raw/rom')
+        shutil.copy(f'{subdir}/{file}', '../app/src/main/res/raw/rom')
         for element in root.iter('string'):
             if 'config_id' in element.attrib.get('name'):
                 element.text = romid
@@ -58,13 +34,10 @@ for subdir in [x[0] for x in os.walk(f'{cwdabspath}/input')]:
                 element.text = romname
             if 'config_core' in element.attrib.get('name'):
                 romcore = element.text
-        tree.write(f'{rootdir}/app/src/main/res/values/config.xml')
+        tree.write('../app/src/main/res/values/config.xml')
         
-        os.chdir(rootdir)
-        os.system(f'{rootdir}/gradlew assembleRelease')
-        os.chdir(cwdabspath)
+        os.chdir('..')
+        os.system('gradlew assembleRelease')
+        os.chdir('autogen')
         
-        shutil.copy(f'{rootdir}/app/build/outputs/apk/release/app-release.apk', f'{cwdabspath}/output/{romcore}_{romid}.apk')
-
-        buildsize -= 1
-shutil.move(f'{cwdabspath}/config.tmp.txt', f'{rootdir}/app/src/main/res/values/config.xml')
+        shutil.copy('../app/build/outputs/apk/release/app-release.apk', f'output/{romcore}_{romid}.apk')
